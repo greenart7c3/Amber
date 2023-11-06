0d0945b317e6530cf0ecb4d963cff31abd4b1af2
greenart7c3 <greenart7c3@proton.me>
2023-11-06 08:56:24 -0300

    fix generate a new key button hidden in navbar

--- app/src/main/java/com/greenart7c3/nostrsigner/ui/LoginScreen.kt
+++ app/src/main/java/com/greenart7c3/nostrsigner/ui/LoginScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -73,6 +74,7 @@ fun LoginPage(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .navigationBarsPadding()
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.SpaceBetween
     ) {

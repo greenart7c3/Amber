8e6b95258009ea6187ee3762934d6981d3794af9
greenart7c3 <greenart7c3@proton.me>
2024-03-22 06:52:08 -0300

    show error screen when not able to parse event json

--- app/src/main/java/com/greenart7c3/nostrsigner/ui/components/SingleEventHomeScreen.kt
+++ app/src/main/java/com/greenart7c3/nostrsigner/ui/components/SingleEventHomeScreen.kt
@@ -1,10 +1,21 @@
 package com.greenart7c3.nostrsigner.ui.components
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import com.greenart7c3.nostrsigner.LocalPreferences
@@ -16,6 +27,7 @@ import com.greenart7c3.nostrsigner.service.AmberUtils
 import com.greenart7c3.nostrsigner.service.IntentUtils
 import com.greenart7c3.nostrsigner.service.getAppCompatActivity
 import com.greenart7c3.nostrsigner.ui.sendResult
+import com.greenart7c3.nostrsigner.ui.theme.ButtonBorder
 import com.vitorpamplona.quartz.encoders.toHexKey
 import com.vitorpamplona.quartz.encoders.toNpub
 import com.vitorpamplona.quartz.events.Event
@@ -196,70 +208,99 @@ fun SingleEventHomeScreen(
         }
 
         else -> {
-            val event = IntentUtils.getIntent(intentData.data, account.keyPair)
-            key = "$packageName-${intentData.type}-${event.kind}"
-            val remember = remember {
-                mutableStateOf(account.savedApps[key] != null)
-            }
-            val shouldRunOnAccept = account.savedApps[key] ?: false
-            EventData(
-                shouldRunOnAccept,
-                remember,
-                packageName,
-                appName,
-                applicationName,
-                event,
-                intentData.data,
-                intentData.type,
-                {
-                    if (event.pubKey != account.keyPair.pubKey.toHexKey()) {
-                        coroutineScope.launch {
-                            Toast.makeText(
-                                context,
-                                context.getString(R.string.event_pubkey_is_not_equal_to_current_logged_in_user),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        return@EventData
-                    }
-
-                    val localEvent = try {
-                        Event.fromJson(intentData.data)
-                    } catch (e: Exception) {
-                        Event.fromJson(event.toJson())
-                    }
-
-                    account.signer.sign<Event>(
-                        localEvent.createdAt,
-                        localEvent.kind,
-                        localEvent.tags,
-                        localEvent.content
-                    ) { signedEvent ->
-                        coroutineScope.launch {
-                            sendResult(
-                                context,
-                                packageName,
-                                account,
-                                key,
-                                remember.value,
-                                clipboardManager,
-                                signedEvent.toJson(),
-                                if (localEvent is LnZapRequestEvent && localEvent.tags.any { tag -> tag.any { t -> t == "anon" } }) signedEvent.toJson() else signedEvent.sig,
-                                intentData
-                            )
-                        }
-                    }
-                },
-                {
-                    if (remember.value) {
-                        account.savedApps[key] = false
-                    }
-
-                    LocalPreferences.saveToEncryptedStorage(account)
-                    context.getAppCompatActivity()?.intent = null
-                    context.getAppCompatActivity()?.finish()
+            val result = runCatching { IntentUtils.getIntent(intentData.data, account.keyPair) }
+            if (result.isFailure) {
+                var showError by remember {
+                    mutableStateOf(false)
                 }
-            )
+                Column(
+                    Modifier.fillMaxSize(),
+                    Arrangement.Center,
+                    Alignment.CenterHorizontally
+                ) {
+                    Text("Error occurred while trying to parse event")
+                    Button(
+                        onClick = {
+                            showError = !showError
+                        },
+                        shape = ButtonBorder
+                    ) {
+                        Text(text = "Show/Hide", color = Color.White)
+                    }
+                    if (showError) {
+                        OutlinedTextField(
+                            value = result.exceptionOrNull()?.message ?: "",
+                            onValueChange = { },
+                            readOnly = true
+                        )
+                    }
+                }
+            } else {
+                val event = result.getOrNull()!!
+                key = "$packageName-${intentData.type}-${event.kind}"
+                val remember = remember {
+                    mutableStateOf(account.savedApps[key] != null)
+                }
+                val shouldRunOnAccept = account.savedApps[key] ?: false
+                EventData(
+                    shouldRunOnAccept,
+                    remember,
+                    packageName,
+                    appName,
+                    applicationName,
+                    event,
+                    intentData.data,
+                    intentData.type,
+                    {
+                        if (event.pubKey != account.keyPair.pubKey.toHexKey()) {
+                            coroutineScope.launch {
+                                Toast.makeText(
+                                    context,
+                                    context.getString(R.string.event_pubkey_is_not_equal_to_current_logged_in_user),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            return@EventData
+                        }
+
+                        val localEvent = try {
+                            Event.fromJson(intentData.data)
+                        } catch (e: Exception) {
+                            Event.fromJson(event.toJson())
+                        }
+
+                        account.signer.sign<Event>(
+                            localEvent.createdAt,
+                            localEvent.kind,
+                            localEvent.tags,
+                            localEvent.content
+                        ) { signedEvent ->
+                            coroutineScope.launch {
+                                sendResult(
+                                    context,
+                                    packageName,
+                                    account,
+                                    key,
+                                    remember.value,
+                                    clipboardManager,
+                                    signedEvent.toJson(),
+                                    if (localEvent is LnZapRequestEvent && localEvent.tags.any { tag -> tag.any { t -> t == "anon" } }) signedEvent.toJson() else signedEvent.sig,
+                                    intentData
+                                )
+                            }
+                        }
+                    },
+                    {
+                        if (remember.value) {
+                            account.savedApps[key] = false
+                        }
+
+                        LocalPreferences.saveToEncryptedStorage(account)
+                        context.getAppCompatActivity()?.intent = null
+                        context.getAppCompatActivity()?.finish()
+                    }
+                )
+            }
         }
     }
 }

e331a0ab93a6e0915698b8145d241a1de9e28c89
greenart7c3 <greenart7c3@proton.me>
2024-04-08 09:07:11 -0300

    move classes out of intentdata file

---
+++ app/src/main/java/com/greenart7c3/nostrsigner/models/CompressionType.kt
@@ -0,0 +1,6 @@
+package com.greenart7c3.nostrsigner.models
+
+enum class CompressionType {
+    NONE,
+    GZIP
+}
---
+++ app/src/main/java/com/greenart7c3/nostrsigner/models/SignerType.kt
@@ -0,0 +1,12 @@
+package com.greenart7c3.nostrsigner.models
+
+enum class SignerType {
+    CONNECT,
+    SIGN_EVENT,
+    NIP04_ENCRYPT,
+    NIP04_DECRYPT,
+    NIP44_ENCRYPT,
+    NIP44_DECRYPT,
+    GET_PUBLIC_KEY,
+    DECRYPT_ZAP_EVENT
+}

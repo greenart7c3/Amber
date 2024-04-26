ec478e65bf7801f904b7490c592f9b8669b1da40
greenart7c3 <greenart7c3@proton.me>
2024-04-26 09:37:19 -0300

    increase timeout when using proxy

--- app/src/main/java/com/greenart7c3/nostrsigner/service/HttpClientManager.kt
+++ app/src/main/java/com/greenart7c3/nostrsigner/service/HttpClientManager.kt
@@ -74,7 +74,7 @@ object HttpClientManager {
         timeout: Duration
     ): OkHttpClient {
         Log.d("HttpClient", "Building client with proxy: ${proxy != null}")
-        val seconds = if (proxy != null) timeout.seconds * 2 else timeout.seconds
+        val seconds = if (proxy != null) timeout.seconds * 3 else timeout.seconds
         val duration = Duration.ofSeconds(seconds)
         return OkHttpClient.Builder()
             .proxy(proxy)

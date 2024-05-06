ce3d927a4be037a51bc9d9d7321aa9ec915d11ba
greenart7c3 <greenart7c3@proton.me>
2024-05-06 15:40:36 -0300

    add scroll to settings screen

--- app/src/main/java/com/greenart7c3/nostrsigner/ui/SettingsScreen.kt
+++ app/src/main/java/com/greenart7c3/nostrsigner/ui/SettingsScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Key
 import androidx.compose.material.icons.filled.Language
@@ -308,7 +310,8 @@ fun SettingsScreen(
     }
 
     Column(
-        modifier,
+        modifier
+            .verticalScroll(rememberScrollState()),
     ) {
         Box(
             Modifier

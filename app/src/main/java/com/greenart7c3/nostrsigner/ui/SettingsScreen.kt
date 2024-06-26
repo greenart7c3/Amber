38d46fe1a7ee46b162ea0df88e1ca1083ae09c39
greenart7c3 <greenart7c3@proton.me>
2024-06-26 16:05:24 -0300

    fix unused import

--- app/src/main/java/com/greenart7c3/nostrsigner/ui/SettingsScreen.kt
+++ app/src/main/java/com/greenart7c3/nostrsigner/ui/SettingsScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons

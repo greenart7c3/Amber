16559c8e8e7ba95e6b9ebc5d4d8d8db672084160
greenart7c3 <greenart7c3@proton.me>
2026-08-14 13:49:04 -0300

    Bump version to 6.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.greenart7c3.nostrsigner"
         minSdk = 26
-        versionCode = 199
-        versionName = "6.5.0"
+        versionCode = 200
+        versionName = "6.5.1"
 
         buildConfigField("boolean", "IS_FDROID_BUILD", "false")
 

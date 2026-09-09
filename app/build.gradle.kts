894f1daf0e53fab3151e48a9462082911380f4a1
greenart7c3 <greenart7c3@proton.me>
2026-09-09 17:15:57 -0300

    Bump version to 6.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.greenart7c3.nostrsigner"
         minSdk = 26
-        versionCode = 203
-        versionName = "6.6.1"
+        versionCode = 204
+        versionName = "6.6.2"
 
         buildConfigField("boolean", "IS_FDROID_BUILD", "false")
 
